7bb7d71568af0b0f66fd1f5007afa73057789d0e
Nikola Yordanov <nikolayordanov2667@gmail.com>
2022-01-12 21:20:45 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const path = require("path");
-app.use(express.json());
-app.use(express.bodyParser({ limit: "50mb" }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
 const io = require("socket.io")(server, {
