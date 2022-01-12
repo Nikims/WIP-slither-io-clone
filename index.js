3b1282d05307b308eb444cea1cbbcb6f5153b3a5
Nikola Yordanov <nikolayordanov2667@gmail.com>
2022-01-12 21:24:03 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:8000",
+    origin: "http://localhost:6952",
     methods: ["GET", "POST"],
     transports: ["websocket"],
     credentials: true,
